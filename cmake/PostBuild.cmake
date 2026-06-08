8c71423c97e042b09ad41e58708f1700eb8e6785
giorgi.page <andreagiorgino1@gmail.com>
2026-06-08 09:20:57 +0200

    cmake refactoring

---
+++ cmake/PostBuild.cmake
@@ -0,0 +1,7 @@
+add_custom_command(
+    TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_directory
+        ${CMAKE_CURRENT_SOURCE_DIR}/include
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>/include
+    COMMENT "Copying headers to build directory..."
+)
