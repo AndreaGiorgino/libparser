d8ab1f66dc097b19910a899bb5fa74e57b0b11a1
giorgi.page <andreagiorgino1@gmail.com>
2026-06-08 09:22:05 +0200

    cmake enable testing

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,6 +1,12 @@
 option(DEBUG "Enable debug build." OFF)
+option(PARSER_BUILD_TESTING "Enable testing and build tests." OFF)
 
-if (DEBUG)
+if(DEBUG)
     add_compile_definitions(DEBUG)
     add_compile_options(-g -ggdb)
+endif()
+
+if(PARSER_BUILD_TESTING)
+    enable_testing()
+    add_subdirectory(ctest)
 endif()
