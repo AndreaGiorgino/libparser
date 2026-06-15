18e55045d6938e6c2bf6f64cab1dd7de7bb07828
giorgi.page <andreagiorgino1@gmail.com>
2026-06-15 10:33:23 +0200

    test stream peek implemented

---
+++ ctest/TestParseFileStreamPeek.cxx
@@ -0,0 +1,38 @@
+#include <format>
+#include <iostream>
+
+#include "libparser/parser.hxx"
+
+static const auto testFilePath { "TestFiles/TestParseFileStream.txt" };
+static const parser::token tokenResult { 0, "#" };
+
+auto TestParseFileStreamPeek(int, char**) -> int {
+    parser p { testFilePath };
+
+    {
+        const auto token { p.peek() };
+        if (token.position != tokenResult.position
+                || token.literal != tokenResult.literal) {
+            std::println(std::cerr,
+                    "-- Token not matching the expected result: "
+                    "\"{{ {}, {:?} }}\" expected, got \"{{ {}, {:?} }}\" instead.",
+                    tokenResult.position, tokenResult.literal,
+                    token.position, token.literal);
+            return 1;
+        }
+    }
+    {
+        const auto token { p.peek() };
+        if (token.position != tokenResult.position
+                || token.literal != tokenResult.literal) {
+            std::println(std::cerr,
+                    "-- Token not matching the expected result: "
+                    "\"{{ {}, {:?} }}\" expected, got \"{{ {}, {:?} }}\" instead.",
+                    tokenResult.position, tokenResult.literal,
+                    token.position, token.literal);
+            return 1;
+        }
+    }
+
+    return 0;
+}
