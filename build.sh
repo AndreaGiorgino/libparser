d32506acc108e9225dd308430771526fcf27f2e2
giorgi.page <andreagiorgino1@gmail.com>
2026-07-14 11:46:22 +0200

    build updated to optionally build the example

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cmake -B build -DPARSER_BUILD_TESTING=ON \
+cmake -B build \
+    -DPARSER_BUILD_TESTING=ON \
+    -DPARSER_BUILD_EXAMPLE=ON \
     && cmake --build build --parallel $(($(nproc) - 1)) \
     && ctest --test-dir build 
